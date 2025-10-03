1a283871d3d24e45c2fcf86c957c9d3fe00fbf7c
Diêgo de Barros <debarrosdiego415@gmail.com>
2025-10-03 03:55:50 -0300

    feat: Adds Customer instance with generated CPF and displays customer data

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
+		/** Sobre o CPF que usei nesta instância de Cliente:
+		 * 
+		 * Instanciando com CPF gerado pelo site https://www.4devs.com.br/gerador_de_cpf, tendo em vista que a implementação do algoritmo de validação de CPF é baseada no algoritmo oficial da Receita Federal do Brasil e causa um erro ao passar quaisquer CPFs inválidos.
+		 */
+		Cliente venilton = new Cliente("554.724.390-60", "Venilton");
 		venilton.setNome("Venilton");
 		
+		System.out.println("=== DADOS DO CLIENTE ===");
+		System.out.println(venilton);
+		System.out.println();
+	
 		Conta cc = new ContaCorrente(venilton);
 		Conta poupanca = new ContaPoupanca(venilton);
 
